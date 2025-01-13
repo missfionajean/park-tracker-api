f15cec760cbbf80402e8c92489ea1c05741c38e6
jstrohsnitter <jstrohsnitter@gmail.com>
2025-01-13 16:32:01 -0500

    commit to stash, many commits behind main

--- django_park_tracker_api/park_tracker_api/serializers.py
+++ django_park_tracker_api/park_tracker_api/serializers.py
@@ -10,4 +10,4 @@ class UserSerializer(serializers.ModelSerializer): # serializers.ModelSerializer
 class TripSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Trip # tell django which model to use
-        fields = ('id', 'park_name', 'times_visited', 'star_rating', 'user_id',) # tell django which fields to include
+        fields = ('id', 'park_name', 'date_visited', 'star_rating', 'user_id',) # tell django which fields to include
